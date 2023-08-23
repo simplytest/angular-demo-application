e612df7afd44f9943cad118b0ff1d237f0e2c782
Robert L.-K <r.lang-kirchhoefer@simplytest.de>
2023-08-23 16:09:32 +0200

    implementing add friend service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from "@angular/core";
 	templateUrl: "./app.component.html",
 	styleUrls: ["./app.component.scss"]
 })
-export class AppComponent  implements OnInit {
+export class AppComponent implements OnInit {
 
 	title = "angular-demo-application";
 
